5d36eca88dfbcaddf64eb6f3f6226e541d973ff7
Taran Raj <taranraj123@gmail.com>
2024-06-11 15:46:12 -0500

    feat: exercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(target_array, ...args) {
+    // What I did before realizing you could pass optional like above
+    // args = Array.from(arguments)
+    // target_array = args[0]
+    // values_to_remove = args.slice(1)
+
+    if (!Array.isArray(target_array)) return "First argument must be an Array"
+
+    return target_array.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line
