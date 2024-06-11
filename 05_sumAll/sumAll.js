2b32b2715344d7b5ab2bae68a25aca0cef4971cc
Taran Raj <taranraj123@gmail.com>
2024-06-11 16:54:29 -0500

    feat: exercise05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    args.sort()
+    if (args.length > 2) {
+        console.error(`Unexpected number of function arguments to sumAll: ${args}`)
+        return "ERROR"
+    } else if (args[0] < 0 || args[1] < 0) {
+        console.error(`Args may not be < 0`)
+        return "ERROR"
+    } else if (typeof(args[0]) !== "number" || typeof(args[1]) !== "number") {
+        console.error(`Args must be numbers`)
+        return "ERROR"
+    }
 
+    let total = 0
+    for (i=args[0]; i<=args[1]; i++) {
+        total += i
+    }
+    return total
 };
 
 // Do not edit below this line
