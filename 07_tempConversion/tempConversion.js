05be88785ea61b556277cc7d1eb8bedc2ed63569
Taran Raj <taranraj123@gmail.com>
2024-06-11 17:08:48 -0500

    feat: exercise07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToN = function(num, n) {
+  return Math.round((num + Number.EPSILON) * 10 ** n) / 10 ** n
+}
+
+const convertToCelsius = function(tempF) {
+  return roundToN((tempF - 32) * 5 / 9, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return roundToN((tempC * 9 / 5) + 32, 1)
 };
 
 // Do not edit below this line
