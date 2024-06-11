83b915f0224a511d83bde200dccdb671e03466a5
Taran Raj <taranraj123@gmail.com>
2024-06-11 11:02:43 -0500

    feat: exercise02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR"
+    let repString = ""
+    for (i=0; i<num; i++){
+        repString += string
+    }
+    return repString
 };
 
 // Do not edit below this line
