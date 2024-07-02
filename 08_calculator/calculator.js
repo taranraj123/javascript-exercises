02c27a48289d7947d986acd371832bfa2375c70d
Taran Raj <taranraj123@gmail.com>
2024-07-02 15:04:24 -0500

    feat: exercise08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, val) => add(acc, val), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, val) => acc * val, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  while (val > 0) {
+    return val * factorial(val - 1)
+  }
+  return 1
 };
 
 // Do not edit below this line
