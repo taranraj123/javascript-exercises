5f565318d55f2239992047d55746b48537760107
Taran Raj <taranraj123@gmail.com>
2024-07-02 15:40:24 -0500

    feat: exercise12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+function age(person) {
+    endYear = person.yearOfDeath === undefined ? (new Date()).getFullYear() : person.yearOfDeath
+    return endYear - person.yearOfBirth
+}
 
+const findTheOldest = function(arr) {
+    return arr.sort((p1, p2) => age(p1) - age(p2)).pop()
 };
 
 // Do not edit below this line
