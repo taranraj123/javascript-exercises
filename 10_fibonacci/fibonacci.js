6d3caf7f68c7aeac81efad71e47f80dbdf1d4b0f
Taran Raj <taranraj123@gmail.com>
2024-07-02 15:26:27 -0500

    feat: exercise10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"
+    if (n == 1) return 1
+    while(n > 0) {
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    }
+    return 0
 };
 
 // Do not edit below this line
